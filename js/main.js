08068127f28f2be88cd07921b37387089a572fd1
Coronel <c_coronel@tss-hr420-dm04.fc.ca>
2017-11-22 16:59:10 -0500

    added the js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+(function () {
+  // start with retrieving the elements from the page, and then adding event handling. then write the logic. refer to the seasons example / homework
+var images = document.querySelectAll(".data-ref"),
+var gallery = document.querySelctor(".thumbInfo"),
+
+//traer e; archivo de carData.js
+
+//function
+
+function cambioText (){
+  //cambiar por var
+  theSubhead.firstChild.nodeValue = objectIndex.headline;
+  theSeasonText.firstChild.nodeValue = objectIndex.text;
+}
+
+})();
